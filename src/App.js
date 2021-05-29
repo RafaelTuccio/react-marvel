a938026f0e838d23ab38ac4cae96aa50b3602d9c
RafaelTuccio <rafael_tuccio@hotmail.com>
2021-05-29 11:20:39 -0300

    getHeroes and getHeroByName

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
+import { useState } from 'react';
+import { getHeroes, getHeroByName } from './service/apiService';
+
 
 function App() {
+  let [name, setName] = useState("");
+
   return (
     <div>
-      
+      <form onSubmit={(event) => {
+          event.preventDefault();
+          getHeroByName({name});
+        }} >
+        <input type="text" value={name} 
+        onChange={(event) => {
+          setName(event.target.value);
+        }}
+        />
+        <button type="submit">submit</button>
+      </form>
     </div>
   );
 }
--- src/service/apiService.js
+++ src/service/apiService.js
@@ -1,1 +1,34 @@
 import axios from 'axios'
+
+
+export const getHeroes = async () => {
+    try{
+        let url = "http://gateway.marvel.com/v1/public/characters?";
+        let limit = "limit=1"
+        let offset = "&offset=1";
+        let orderBy = "&orderBy=-name"
+        let ts = "&ts=1622054338";
+        let apikey = "&apikey=924ede14a9e9d54b76b557eb701ddf4c";
+        let hash = "&hash=0ddec45d15e5daf65919c9ef98e48a06";
+        let heroes = await axios.get(url + orderBy + ts + apikey + hash)
+        console.log(url + limit + offset + ts + apikey + hash)
+        console.log(heroes);
+    }catch(err){
+        console.error(err)
+    }
+}
+
+export const getHeroByName = async ({name}) =>{
+    try{
+        let url = "http://gateway.marvel.com/v1/public/characters?";
+        //let name = "name=spider-man"
+        let ts = "&ts=1622054338";
+        let apikey = "&apikey=924ede14a9e9d54b76b557eb701ddf4c";
+        let hash = "&hash=0ddec45d15e5daf65919c9ef98e48a06";
+        let heroes = await axios.get(url + "name=" + name + ts + apikey + hash)
+        console.log(url + name + ts + apikey + hash)
+        console.log(heroes);
+    }catch(err){
+        console.log(err)
+    }
+}
