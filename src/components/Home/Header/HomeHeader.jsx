eb2d8fb85bfb0b4fde3159c0999687c01871c055
RafaelTuccio <rafael_tuccio@hotmail.com>
2021-06-04 23:45:55 -0300

    ajeitando o Header

--- src/components/Home/Header/HomeHeader.jsx
+++ src/components/Home/Header/HomeHeader.jsx
@@ -6,7 +6,7 @@ const HomeHeader = () => {
         <header className="header-home">
             <div className="logo-maior">
                 <img src={logo} alt="logo marvel" />
-                <h1>EXPORE O UNIVERSO</h1>
+                <h1>EXPLORE O UNIVERSO</h1>
                 <span className="header-subtitulo">Mergulhe no domínio deslumbrante de todos os personagens clássicos que você ama - e aqueles que você descobrirá em breve!</span>
             </div>
         </header>
